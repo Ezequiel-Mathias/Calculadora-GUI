00492a456eba29451db437411159b50678ae65d9
ezequiel <ezequielmathias32@gmail.com>
2021-10-28 16:46:12 -0300

    falta a tabuada e a conta

---
+++ src/br/com/senai/App.java
@@ -0,0 +1,12 @@
+package br.com.senai;
+
+import br.com.senai.ui.FrameTela;
+
+public class App {
+
+	public static void main(String[] args) {
+		FrameTela minhatela = new FrameTela();
+		minhatela.telacalculadora();
+	}
+
+}
---
+++ src/br/com/senai/ui/contas/ContasCalculadora.java
@@ -0,0 +1,24 @@
+package br.com.senai.ui.contas;
+
+
+
+public class ContasCalculadora {
+	
+	int contador = 0;
+	
+	public void calculo () {
+		
+				
+		do {
+					
+		System.out.println("1");
+					
+		}while(contador < 4);
+				
+
+}
+	
+		
+}
+	
+
